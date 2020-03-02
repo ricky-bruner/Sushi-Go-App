f989a489588718e5b2af48ea82d45f695e5c3f1d
ricky-bruner <alteredecho07@yahoo.com>
2020-03-01 20:19:31 -0600

    added images and corner assests

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './img/wasabi.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testing</h1>
+      <div className="card-container">
+        <div className="card">
+          <div className="wasabi-card">
+            <img src={logo} className="main-card-img" alt="wasabi" />
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
